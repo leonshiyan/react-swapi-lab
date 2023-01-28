a468d61f5782ea77fc72c94dbc90557de78e53ee
leonshiyan <leonshiyan@gmail.com>
2023-01-27 16:43:43 -0800

    add even more style

--- src/pages/ShipDetails/ShipDetails.jsx
+++ src/pages/ShipDetails/ShipDetails.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 import { getDetails } from '../../services/sw-api'
 import { Link } from 'react-router-dom'
+import './ShipDetails.css'
 
 const ShipDetails = () => {
   const [shipDetails, setShipDetails] = useState({})
@@ -17,12 +18,12 @@ const ShipDetails = () => {
   }, [location.state.ship.url])
 
   return ( 
-    <>
-      <h3>Ship Deets</h3>
+    <div className='shipCard'>
       <h3>NAME: {shipDetails.name}</h3>
-      <h4>MODEL: {shipDetails.model}</h4>
-      <div><Link to='/'>RETURN</Link></div>
-    </>
+      <h5>MODEL: {shipDetails.model}</h5>
+      <Link to='/'>RETURN</Link>
+    </div>
+    
   )
 }
 
